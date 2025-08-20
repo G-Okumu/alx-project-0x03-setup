9d343ee88c9c4f8c35b253b1a2df2fcfe67a5f8f
George-Okumu <gokumu12@gmail.com>
2025-08-20 14:27:20 +0300

    ref: re-organized the imported interfaces

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -1,7 +1,7 @@
+import { PageRouteProps } from "@/interface";
 
 import { useRouter } from "next/router";
 import Button from "@/components/common/Button";
-import { type PageRouteProps } from "@/interface";
 
 export default function Home() {
   const router = useRouter();
